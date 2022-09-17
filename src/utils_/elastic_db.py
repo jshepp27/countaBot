cdcbf0f4c9634be038c9d269b1ec232e33a22f2d
jshepp27 <j.sheppard.ldn@gmail.com>
2022-09-17 14:20:38 +0100

    small refactor to ranker retriever

--- src/utils_/elastic_db.py
+++ src/utils_/elastic_db.py
@@ -21,11 +21,11 @@ class ElasticDB(object):
             :param elastic_fields: Search fields
         """
         # Connect
-        logger.info('Connecting to %s ' % elastic_port)    
-        self.elastic_port = elastic_port
-        self.es = Elasticsearch(elastic_port, retry_on_timeout=True)
-        self.elastic_index = elastic_index
+        logger.info('Connecting to %s ' % elastic_port)
 
+        self.es = Elasticsearch(elastic_port, retry_on_timeout=True)
+        self.elastic_port = elastic_port
+        self.es.index = elastic_index
         logger.info('Connected to %s ' % self.es)
 
     def add_doc(self, doc):
@@ -37,12 +37,13 @@ class ElasticDB(object):
 
         logger.info('Added document id %s' % response["_id"])
 
-    def bulk_add(self, index_name, source, iterator, chunk_size):
+    def bulk_add(self, index_name, source, iterator, chunk_size, len_):
         
         errors_before_interrupt = 5
         successes = 0
+        errors_count = 0
         
-        for ok, result in parallel_bulk(self.es, iterator(idx=index_name, source=source), chunk_size=chunk_size, request_timeout=60*3):
+        for ok, result in parallel_bulk(self.es, iterator(idx=index_name, source=source, len_=len_), chunk_size=chunk_size, request_timeout=60*3):
             if ok is not True:
                 logger.error('Failed to import data')
                 logger.error(str(result))
@@ -53,7 +54,6 @@ class ElasticDB(object):
                 exit(1)
             
             successes += ok
-
 
     def search(self, query_, k=5):
         results = self.es.search(
@@ -66,44 +66,15 @@ class ElasticDB(object):
             size=k)
 
         hits = results["hits"]["hits"]
-        #doc_ids = [row['_source']["document"]["id"] for row in hits]
 
         return hits
 
-        # results = self.es.search(
-        #     index=self.elastic_index,
-        #     body= {
-        #         "size": k,
-        #         "query": {
-        #             "match": {
-        #                 "text": query,
-        #     }}})
-
-        # hits = results["hits"]["hits"]
-        # doc_ids = [row['_source']["id"] for row in hits]
-
-        # return (hits, doc_ids)
-
     # Elasticsearch Controls
-
     def __enter__(self):
         return self
 
     def __close__(self):
         self.es = None
-
-    # def get_doc_index(self, doc_id, field_doc_name):
-    #     """Convert doc_id --> doc_index"""
-    #     field_index = field_doc_name
-    #     if isinstance(field_index, list):
-    #         field_index = '.'.join(field_index)
-    #     result = self.es.search(index=self.elastic_index, body={'query':{'match':
-    #         {field_index: doc_id}}})
-    #     return result['hits']['hits'][0]['_id']
-
-    # def get_doc_text(self, doc_id):
-    #     """ Fetch doc text for given doc_id """
-    #     idx = self.get_doc_index(doc_id)
 
 
 
