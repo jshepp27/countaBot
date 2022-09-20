8ba0b36da7ebe90d0cdfb4467a7e16fd609f518a
jshepp27 <j.sheppard.ldn@gmail.com>
2022-09-20 21:50:51 +0100

    Refactored and enhanced knowledgebase construction. Reintitialising knowledgebases.

--- src/utils_/elastic_db.py
+++ src/utils_/elastic_db.py
@@ -1,10 +1,10 @@
 import logging
 import os
+from tqdm import tqdm
 
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import streaming_bulk, parallel_bulk
 from tqdm.auto import tqdm
-
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 class ElasticDB(object):
 
-    def __init__(self, elastic_port=None, elastic_index=None):
+    def __init__(self, elastic_port=None):
         """
         Args:
             :param elastic_port: Port for Elasticsearch instance
@@ -25,8 +25,15 @@ class ElasticDB(object):
 
         self.es = Elasticsearch(elastic_port, retry_on_timeout=True)
         self.elastic_port = elastic_port
-        self.es.index = elastic_index
         logger.info('Connected to %s ' % self.es)
+
+    def add_index(self, index):
+        self.es.indices.create(index=index)
+        logger.info('Connected to %s ' % index)
+
+    def set_index(self, index):
+        self.es.index = index
+        logger.info('Set Index to to %s ' % self.es.index)
 
     def add_doc(self, doc):
         """ Add doc to DB """
@@ -37,23 +44,24 @@ class ElasticDB(object):
 
         logger.info('Added document id %s' % response["_id"])
 
-    def bulk_add(self, file, index_name, source, iterator, chunk_size, len_):
+    def bulk_add(self, files, index_name, generator, source, iterator, chunk_size, len_):
         
         errors_before_interrupt = 5
         successes = 0
         errors_count = 0
-        
-        for ok, result in parallel_bulk(self.es, iterator(file=file, idx=index_name, source=source, len_=len_), chunk_size=chunk_size, request_timeout=60*3):
-            if ok is not True:
-                logger.error('Failed to import data')
-                logger.error(str(result))
-                errors_count += 1
 
-            if errors_count == errors_before_interrupt:
-                logging.fatal('Too many import errors, exiting with error code')
-                exit(1)
-            
-            successes += ok
+        with tqdm(total=(len(files)), position=0, leave=True):
+            for ok, result in parallel_bulk(self.es, iterator(files=files, idx=index_name, generator=generator, source=source), chunk_size=chunk_size, request_timeout=60*3):
+                if ok is not True:
+                    logger.error('Failed to import data')
+                    logger.error(str(result))
+                    errors_count += 1
+
+                if errors_count == errors_before_interrupt:
+                    logging.fatal('Too many import errors, exiting with error code')
+                    exit(1)
+
+                successes += ok
 
     def search(self, query_, k=5):
         results = self.es.search(
